c09c4392f2cefb927a11de3fb768ef61c3b748a7
Romain Forlot <romain.forlot@iot.bzh>
2017-02-10 13:50:59 +0000

    Initialize CanBus_Handler
    Also added licence
    
    Change-Id: I629624186f720ca4fa9b7a4f91d014e0bc02b0db
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- low-can-binding.cpp
+++ low-can-binding.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015, 2016 "IoT.bzh"
+ * Author "Romain Forlot" <romain.forlot@iot.bzh>
+ * Author "Loic Collignon" <loic.collignon@iot.bzh>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <unistd.h>
@@ -289,6 +305,7 @@ int afbBindingV1ServiceInit(struct afb_service service)
 	/* Open JSON conf file */
 
 	/* Open CAN socket */
+	CanBus CanBus_handler;
 	CanBus_handler.open();
     CanBus_handler.start_threads();
 
