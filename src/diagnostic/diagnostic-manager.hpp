1da3cb296788fa0a32f3669edf4c2539e80ad2e2
Romain Forlot <romain.forlot@iot.bzh>
2017-03-09 10:16:14 +0100

    Format
    Change-Id: Ie9159b039e468e511b497884816453093048aa80
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- src/diagnostic/diagnostic-manager.hpp
+++ src/diagnostic/diagnostic-manager.hpp
@@ -38,40 +38,40 @@
  * the diagnostics library to the VI's CAN peripheral.
  */
 class diagnostic_manager_t {
-	protected:
+protected:
 	static void shims_logger(const char* m, ...);
 	static void shims_timer();
 
-	private:
-		DiagnosticShims shims_; /*!< shims_ - An array of shim functions for each CAN bus that plug the diagnostics
-								 * library (uds-c) into the VI's CAN peripheral.*/
-		can_bus_dev_t& bus_; /*!< bus_ - A reference to the CAN bus that should be used for all standard OBD-II requests, if the bus is not
-							  * explicitly spcified in the request. If NULL, all requests require an explicit bus.*/
-		std::queue<active_diagnostic_request_t> recurringRequests_; /*!< recurringRequests - A queue of active, recurring diagnostic requests. When
-																  * a response is received for a recurring request or it times out, it is
-																  * popped from the queue and pushed onto the back. */
-		std::vector<active_diagnostic_request_t> nonrecurringRequests_; /*!< nonrecurringRequests - A list of active one-time diagnostic requests. When a
-																	  * response is received for a non-recurring request or it times out, it is
-																	  * removed from this list and placed back in the free list.*/
-		std::vector<active_diagnostic_request_t> freeRequestEntries_; /*!< freeRequestEntries - A list of all available slots for active diagnostic
-																	* requests. This free list is backed by statically allocated entries in
-																	* the requestListEntries attribute.*/
-		std::vector<active_diagnostic_request_t> requestListEntries_[50]; /*!< requestListEntries - Static allocation for all active diagnostic requests.*/
+private:
+	DiagnosticShims shims_; /*!< shims_ - An array of shim functions for each CAN bus that plug the diagnostics
+								* library (uds-c) into the VI's CAN peripheral.*/
+	can_bus_dev_t& bus_; /*!< bus_ - A reference to the CAN bus that should be used for all standard OBD-II requests, if the bus is not
+							* explicitly spcified in the request. If NULL, all requests require an explicit bus.*/
+	std::queue<active_diagnostic_request_t> recurringRequests_; /*!< recurringRequests - A queue of active, recurring diagnostic requests. When
+																* a response is received for a recurring request or it times out, it is
+																* popped from the queue and pushed onto the back. */
+	std::vector<active_diagnostic_request_t> nonrecurringRequests_; /*!< nonrecurringRequests - A list of active one-time diagnostic requests. When a
+																	* response is received for a non-recurring request or it times out, it is
+																	* removed from this list and placed back in the free list.*/
+	std::vector<active_diagnostic_request_t> freeRequestEntries_; /*!< freeRequestEntries - A list of all available slots for active diagnostic
+																* requests. This free list is backed by statically allocated entries in
+																* the requestListEntries attribute.*/
+	std::vector<active_diagnostic_request_t> requestListEntries_[50]; /*!< requestListEntries - Static allocation for all active diagnostic requests.*/
 
-		bool initialized_; /*!< * initialized - True if the DiagnosticsManager has been initialized with shims. It will interface with the uds-c lib*/
+	bool initialized_; /*!< * initialized - True if the DiagnosticsManager has been initialized with shims. It will interface with the uds-c lib*/
 
-	public:
-		diagnostic_manager_t(can_bus_dev_t& bus);
-		void init_diagnostic_shims();
+public:
+	diagnostic_manager_t(can_bus_dev_t& bus);
+	void init_diagnostic_shims();
 
-		void checkSupportedPids(const active_diagnostic_request_t& request,
-		const DiagnosticResponse& response, float parsedPayload);
+	void checkSupportedPids(const active_diagnostic_request_t& request,
+	const DiagnosticResponse& response, float parsedPayload);
 
-		bool addRecurringRequest(DiagnosticRequest* request, const char* name,
-			bool waitForMultipleResponses, const DiagnosticResponseDecoder decoder,
-			const DiagnosticResponseCallback callback, float frequencyHz);
+	bool addRecurringRequest(DiagnosticRequest* request, const char* name,
+		bool waitForMultipleResponses, const DiagnosticResponseDecoder decoder,
+		const DiagnosticResponseCallback callback, float frequencyHz);
 
-		void reset();
+	void reset();
 
-		void add_request(int pid);
+	void add_request(int pid);
 };
