1a67a9c8647c7fa48ee435f033f336e5f83ac270
Romain Forlot <romain.forlot@iot.bzh>
2017-04-21 09:17:31 +0200

    Make virtual CAN device name as argument
    Change-Id: I6ae48b36171fbc038c9a3a8841d9dbadf7686ee3
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- CAN-binder/can_samples/init_vcan.sh
+++ CAN-binder/can_samples/init_vcan.sh
@@ -2,5 +2,5 @@
 # example for setting up vcan0 and modules
 sudo modprobe can
 sudo modprobe vcan
-sudo ip link add dev can0 type vcan
-sudo ip link set up can0
+sudo ip link add dev $1 type vcan
+sudo ip link set up $1
