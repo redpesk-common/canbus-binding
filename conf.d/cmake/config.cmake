61850844fb8213d316fa2d356219d47079cf7fe5
Romain Forlot <romain.forlot@iot.bzh>
2017-07-05 18:36:17 +0200

    Can't set compile FLAG using set using Yocto build
    If set, then these are the flags that are used by bitbake
    without append its own flags which provides toolchain variables
    environment mandatory to correctly build for another target.
    
    Change-Id: Id5ddfc11c3d7d2e1eee418989aabaa73a933a41a
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- conf.d/cmake/config.cmake
+++ conf.d/cmake/config.cmake
@@ -70,10 +70,8 @@ set (PKG_REQUIRED_LIST
 # -----------------------------
 add_compile_options(-D_REENTRANT)
 add_compile_options(-DPB_FIELD_16BIT)
-
-# LANG Specific compile flags set for all build types
-set(CMAKE_C_FLAGS "")
-set(CMAKE_CXX_FLAGS "-std=c++11 -pthread")
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-std=c++11>)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-pthread>)
 
 # Print a helper message when every thing is finished
 # ----------------------------------------------------
