df5d52a36b2d6ac9f3bea84ac7e38a10f8bed7f7
Romain Forlot <romain.forlot@iot.bzh>
2020-10-06 14:18:05 +0200

    Remove nmea2k dedicated decoders
    Remove nmea2k dedicated decoders which should lies in the plugin code itself.
    
    Change-Id: I07a0dd1d811be22ccb712e283895f2347276eef3
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- include/low-can/can/can-decoder.hpp
+++ include/low-can/can/can-decoder.hpp
@@ -35,9 +35,6 @@ public:
 	static openxc_DynamicField decode_ascii(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
 	static openxc_DynamicField decode_date(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
 	static openxc_DynamicField decode_time(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
-	static openxc_DynamicField decode_60160(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
-	static openxc_DynamicField decode_60416(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
-	static openxc_DynamicField decode_fast_packet(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
 
 	static openxc_DynamicField translate_signal(signal_t& signal, std::shared_ptr<message_t> message, bool* send);
 
